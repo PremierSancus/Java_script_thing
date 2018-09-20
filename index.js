c55a96883ceb6f3045e5b33e7a537e3adc4ac05e
PremierRemar <r.bacadon@share.epsb.ca>
2018-09-20 12:54:32 -0600

    update

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 console.log('Hello World');
 
-var name = "mark";
+let FirstName = "Remar";
+let SecondName = "Gabriel";
+let MiddleName = "Espaldon";
+let LastName = "Bacadon";
+let EyeColor = "Black Eyes";
+let HairColor = "Black";
+let BodyAppearance = "Fat";
+let Age = "16";
+let MadeIn = "Phillippines";
+let Author = "Parents";
+let Height = "5 feet, 8 inches";
+let SocialSkill = "Basic";
+let Weight = "210"
+const agewas = 16
+
 console.log(name);
