cff0139b0afce05ebb67fc5d82267a9a0ccd1147
PremierRemar <r.bacadon@share.epsb.ca>
2018-09-20 13:10:01 -0600

    update

---
+++ Swapping Variables/index.js
@@ -0,0 +1,35 @@
+console.log('Hello World');
+
+let FirstName = "Remar";
+let SecondName = "Gabriel";
+let MiddleName = "Espaldon";
+let LastName = "Bacadon";
+let EyeColor = "Black Eyes";
+let HairColor = "Black";
+let BodyAppearance = "Fat";
+let Age = "16";
+let MadeIn = "Phillippines";
+let Author = "Parents";
+let Height = "5 feet, 8 inches";
+let SocialSkill = null; //undefined,null
+let Weight = "210"
+const Male = true; //security feature
+let Asian = true;
+let spouse ="none"
+
+console.log(FirstName);
+console.log(SecondName);
+console.log(MiddleName);
+console.log(LastName);
+console.log(EyeColor);
+console.log(HairColor);
+console.log(BodyAppearance);
+console.log(Age);
+console.log(MadeIn);
+console.log(Author);
+console.log(Height);
+console.log(SocialSkill);
+console.log(Weight);
+console.log("Am I manly?" + Male);
+console.log("Am I Asian" + Asian);
+console.log(spouse);
