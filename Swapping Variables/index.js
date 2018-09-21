c3e5dd7d259a10f7838a970f5cfce5294fc18c1c
PremierRemar <r.bacadon@share.epsb.ca>
2018-09-21 13:07:39 -0600

    update

--- Swapping Variables/index.js
+++ Swapping Variables/index.js
@@ -4,5 +4,5 @@ let LastName = "Bacadon";
 //new code goes here
 
 //print variables to console
-console.log("Mi primero nombre es" + LastName);
-console.log("Mi Segundo nombre es" + FirstName);
+console.log("Mi primero nombre es " + LastName);
+console.log("Mi segundo nombre es " + FirstName);
