761b92a58e7b23cf733475000956393f40cbc24c
PremierRemar <r.bacadon@share.epsb.ca>
2018-09-21 13:30:41 -0600

    update

---
+++ Swapping Variables Object Start/index.js
@@ -0,0 +1,24 @@
+console.log('Hello World');
+
+let RemarBacadon = {
+  FirstName: "Remar",
+  SecondName: "Gabriel",
+  MiddleName: "Espaldon",
+  LastName: "Bacadon",
+  EyeColor: "Black Eyes",
+  HairColor: "Black",
+  BodyAppearance: "Fat",
+  Age: '16',
+  MadeIn: "Phillippines",
+  Author: "Parents",
+  Height: "5 feet, 8 inches",
+  SocialSkill: null,
+  Weight: "210",
+  male: true,
+  female: false,
+  Asian: true,
+  spouse: "none",
+}
+
+
+console.log(RemarBacadon);
